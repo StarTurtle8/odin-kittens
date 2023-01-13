29ab6dfcf4218be7216962d0c6798411e7997ba7
StarTurtle8 <william.estvanik@gmail.com>
2023-01-13 12:01:08 -0800

    add RESTful

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,54 @@
+class KittensController < ApplicationController
+  def new
+    @kitten = Kitten.new
+  end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+
+    if @kitten.save
+      flash[:notice] = "Kitten summoned"
+      redirect_to @kitten
+    else
+      flash.now[:error] = "Could not summon kitten"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def index
+    @kittens = Kitten.all
+  end
+
+  def show
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def edit
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def update
+    @kitten = Kitten.find(params[:id])
+
+    if @kitten.update(kitten_params)
+      flash[:notice] = "Kitten re-summoned"
+      redirect_to @kitten
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @kitten = Kitten.find(params[:id])
+    @kitten.destroy
+    flash[:alert] = 'Kitten Destroyed'
+
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  get 'kittens/new'
+  get 'kittens/create'
+  get 'kittens/index'
+  get 'kittens/show'
+  get 'kittens/edit'
+  get 'kittens/update'
+  get 'kittens/delete'
+  resources :kittens
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "kittens#index"
 end
